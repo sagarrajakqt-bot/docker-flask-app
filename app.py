99762c54746c876c3a5cbceba7e948f8b3099db5
sagarrajak <sagar.rajak.qt@gmail.com>
2026-04-21 09:56:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,5 @@ def health():
     return {"status": "ok"}
 
 import os
-if __name__ == "__main__":
-
 port = int(os.environ.get("PORT", 5000))
 app.run(host="0.0.0.0", port=port)
->>>>>>> 94aa5a85306f95f84bb1b97581e58dfd54253690
